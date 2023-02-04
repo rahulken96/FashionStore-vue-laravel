35620fed5107d0dde6d20237556813dc8201440f
RahulKen96 <rahulKen96@gmail.com>
2023-02-04 12:04:58 +0700

    [First Commit] Vue Views

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderFashstore />
+    <HeroFashstore />
+    <BannerFashstore />
+    <InstagramFashstore />
+    <PartnerFashstore />
+    <FooterFashstore />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderFashstore from "@/components/HeaderFashstore.vue";
+import HeroFashstore from "@/components/HeroFashstore.vue";
+import BannerFashstore from "@/components/BannerFashstore.vue";
+import InstagramFashstore from "@/components/InstagramFashstore.vue";
+import PartnerFashstore from "@/components/PartnerFashstore.vue";
+import FooterFashstore from "@/components/FooterFashstore.vue";
+
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderFashstore,
+    HeroFashstore,
+    BannerFashstore,
+    InstagramFashstore,
+    PartnerFashstore,
+    FooterFashstore,
+  },
+};
+</script>
