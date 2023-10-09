1963a557d34c221cae2f1c846fc4f84823e57ff6
RahulKen96 <rahulKen96@gmail.com>
2023-10-09 20:46:19 +0700

    [Update] Statisktik Report Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -14,7 +15,15 @@ class DashboardController extends Controller
     public function index()
     {
         $data = [
-            'dashboard' => 'active',
+            'dashboard'     => 'active',
+            'pendapatan'    => Transaksi::where('status_transaksi', 1)->sum('total_transaksi'),
+            'penjualan'     => Transaksi::count(),
+            'trxTerkahir'   => Transaksi::orderBy('transaksi_id', 'DESC')->limit(5)->get(),
+            'pie'           => [
+                'PENDING'   => Transaksi::where('status_transaksi', 0)->count(),
+                'SUKSES'    => Transaksi::where('status_transaksi', 1)->count(),
+                'GAGAL'     => Transaksi::where('status_transaksi', 2)->count(),
+            ],
         ];
 
         return view('pages.dashboard', $data);
