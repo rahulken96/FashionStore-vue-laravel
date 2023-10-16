5a8dcd9adfdf85ce7b7184153983bec1e3df39a6
RahulKen96 <rahulKen96@gmail.com>
2023-10-16 08:25:29 +0700

    [Update] API Produk

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -26,24 +26,12 @@
             <div class="row">
               <div class="col-lg-6">
                 <div class="product-pic-zoom">
-                  <img class="product-big-img" :src="'img/'+gambar_default" alt="" />
+                  <img class="product-big-img" :src="gambar_default" alt="" />
                 </div>
-                <div class="product-thumbs">
+                <div class="product-thumbs" v-if="detail_produk.galeri.length > 0">
                   <carousel class="product-thumbs-track ps-slider" :nav="false" :dots="false" :item="3">
-                    <div class="pt" @click="'img/'+ubahGambar(thumbs[0])" :class="thumbs[0] == gambar_default? 'active' : '' ">
-                      <img src="img/mickey1.jpg" alt="" />
-                    </div>
-
-                    <div class="pt" @click="'img/'+ubahGambar(thumbs[1])" :class="thumbs[1] == gambar_default? 'active' : '' ">
-                      <img src="img/mickey2.jpg" alt="" />
-                    </div>
-
-                    <div class="pt" @click="'img/'+ubahGambar(thumbs[2])" :class="thumbs[2] == gambar_default? 'active' : '' ">
-                      <img src="img/mickey3.jpg" alt="" />
-                    </div>
-
-                    <div class="pt" @click="'img/'+ubahGambar(thumbs[3])" :class="thumbs[3] == gambar_default? 'active' : '' ">
-                      <img src="img/mickey4.jpg" alt="" />
+                    <div v-for="foto in detail_produk.galeri" :key="foto.produk_galeri_id" class="pt" @click="ubahGambar(foto.foto_produk)" :class="foto.foto_produk == gambar_default? 'active' : '' ">
+                      <img :src="foto.foto_produk" alt="Foto Produk" />
                     </div>
                   </carousel>
                 </div>
@@ -51,33 +39,13 @@
               <div class="col-lg-6">
                 <div class="product-details text-left">
                   <div class="pd-title">
-                    <span>Bag</span>
-                    <h3>Mickey Tote Bag</h3>
+                    <span>{{ detail_produk.tipe_produk }}</span>
+                    <h3>{{ detail_produk.nama_produk }}</h3>
+                  </div>
+                  <div v-html="detail_produk.deskripsi_produk">
                   </div>
                   <div class="pd-desc">
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Corporis, error officia. Rem aperiam laborum voluptatum
-                      vel, pariatur modi hic provident eum iure natus quos non a
-                      sequi, id accusantium! Autem.
-                    </p>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                      Quam possimus quisquam animi, commodi, nihil voluptate
-                      nostrum neque architecto illo officiis doloremque et
-                      corrupti cupiditate voluptatibus error illum. Commodi
-                      expedita animi nulla aspernatur. Id asperiores blanditiis,
-                      omnis repudiandae iste inventore cum, quam sint molestiae
-                      accusamus voluptates ex tempora illum sit perspiciatis.
-                      Nostrum dolor tenetur amet, illo natus magni veniam quia
-                      sit nihil dolores. Commodi ratione distinctio harum
-                      voluptatum velit facilis voluptas animi non laudantium, id
-                      dolorem atque perferendis enim ducimus? A exercitationem
-                      recusandae aliquam quod. Itaque inventore obcaecati, unde
-                      quam impedit praesentium veritatis quis beatae ea atque
-                      perferendis voluptates velit architecto?
-                    </p>
-                    <h4>$495.00</h4>
+                    <h4>Rp {{ detail_produk.harga_produk }}</h4>
                   </div>
                   <div class="quantity">
                     <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
@@ -102,6 +70,7 @@ import FooterFashstore from "@/components/FooterFashstore.vue";
 import RelatedProducts from "@/components/RelatedProducts.vue";
 
 import carousel from "vue-owl-carousel";
+import axios from "axios";
 
 export default {
   name: "ProductView",
@@ -113,20 +82,36 @@ export default {
   },
   data (){
     return {
-      gambar_default : "mickey2.jpg",
+      gambar_default : "",
       thumbs : [
         "mickey1.jpg",
         "mickey2.jpg",
         "mickey3.jpg",
         "mickey4.jpg",
-      ]
+      ],
+      detail_produk : [],
     }
   },
   methods : {
     ubahGambar(url){
       this.gambar_default = url;
+    },
+    setDataGambar(data){
+      this.detail_produk = data;
+      this.gambar_default = data.galeri[0].foto_produk;
     }
-  }
+  },
+  mounted() {
+    axios
+    .get("http://127.0.0.1:8000/api/produk", {
+      params: {
+        id: this.$route.params.id,
+      }
+    })
+    .then(res => (this.setDataGambar(res.data.data)))
+    // eslint-disable-next-line no-console
+    .catch(err => console.log(err));
+  },
 };
 </script>
 <style scoped>
